12db86480a665525e53b38ed9dafd191bf8873f8
Jon Rovira <jonathanrovira2016@u.northwestern.edu>
2016-05-23 13:27:31 -0500

    initial commit, webpack set up

---
+++ webpack.config.js
@@ -0,0 +1,139 @@
+const path = require('path');
+const merge = require('webpack-merge');
+const webpack = require('webpack');
+const NpmInstallPlugin = require('npm-install-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanPlugin = require('clean-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+
+
+const pkg = require('./package.json');
+
+
+
+const TARGET = process.env.npm_lifecycle_event;
+const PATHS = {
+	app: path.join(__dirname, 'app'),
+	build: path.join(__dirname, 'build'),
+	style: path.join(__dirname, 'app/main.css')
+};
+
+
+
+process.env.BABEL_ENV = TARGET;
+
+
+
+// common to both "npm run start" and "npm run build"
+const common = {
+	entry: {
+		app: PATHS.app,
+		style: PATHS.style
+	},
+	resolve: {
+		extensions: ['', '.js', '.jsx']
+	},
+	output: {
+		path: PATHS.build,
+		filename: '[name].js'
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.jsx?$/,
+				loaders: ['babel?cacheDirectory'],
+				include: PATHS.app
+			}
+		]
+	},
+	plugins: [
+		// dynamically generate index file
+		new HtmlWebpackPlugin({
+			template: 'node_modules/html-webpack-template/index.ejs',
+			title: 'Divvy Predictions',
+			appMountId: 'app',
+			inject: false
+		})
+	]
+};
+
+
+
+if (TARGET === 'start' || !TARGET) {
+	module.exports = merge(common, {
+		devtool: 'eval-source-map',
+		devServer: {
+			historyApiFallback: true,
+			hot: true,
+			inline: true,
+			progress: true,
+			stats: 'errors-only',
+			host: process.env.HOST,
+			port: process.env.PORT
+		},
+		module: {
+			loaders: [
+				{
+					test: /\.css$/,
+					loaders: ['style', 'css'],
+					include: PATHS.style
+				}
+			]
+		},
+		plugins: [
+			new webpack.HotModuleReplacementPlugin(),
+      		new NpmInstallPlugin({
+        		save: true // --save
+      		})
+		]
+	});
+}
+
+
+
+
+if (TARGET === 'build' || TARGET === 'stats') {
+	module.exports = merge(common, {
+		entry: {
+			vendor: Object.keys(pkg.dependencies).filter(function (v) {
+				return v !== 'alt-utils';
+			})
+		},
+		output: {
+			path: PATHS.build,
+			filename: '[name].[chunkhash].js',
+			chunkFilename: '[chunkhash].js'
+		},
+		module: {
+			loaders: [
+				{
+					test: /\.css$/,
+					loader: ExtractTextPlugin.extract('style', 'css'),
+					include: PATHS.style
+				}
+			]
+		},
+		plugins: [
+			// clean build directory
+			new CleanPlugin([PATHS.build], {
+				verbose: false
+			}),
+			new ExtractTextPlugin('[name].[chunkhash].css'),
+			new webpack.optimize.CommonsChunkPlugin({
+				names: ['vendor', 'manifest']
+			}),
+			// removes react code like prop type checking
+			new webpack.DefinePlugin({
+				'process.env.NODE_ENV': '"production"'
+			}),
+			new webpack.optimize.UglifyJsPlugin({
+				compress: {
+					warnings: false
+				}
+			})
+		]
+	});
+}
+
+
