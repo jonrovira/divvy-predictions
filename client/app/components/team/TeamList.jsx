a463a84b31d04d45bd8966221d597d57f3511b05
Jon Rovira <jonathanrovira2016@u.northwestern.edu>
2016-06-08 21:17:43 -0500

    attempt to show tableau on gh-pages

--- client/app/components/team/TeamList.jsx
+++ client/app/components/team/TeamList.jsx
@@ -5,7 +5,19 @@ export default class TeamList extends React.Component {
 
 
 	render() {
-		return (<h1>Hi</h1>);
+		return (
+			<ul className="team-list">
+				<li>
+					<h3>Mark Giancreco</h3>
+				</li>
+				<li>
+					<h3>Christopher Mark</h3>
+				</li>
+				<li>
+					<h3>Jon Rovira</h3>
+				</li>
+			</ul>
+		);
 	}
 
 
