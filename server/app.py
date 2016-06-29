ac9539c1814baef496727c5abdc4c9dfdba41ca1
Jon Rovira <jonathanrovira2016@u.northwestern.edu>
2016-06-29 17:16:44 -0500

    trying to add the specific origin for cors

--- server/app.py
+++ server/app.py
@@ -16,7 +16,7 @@ predictor = Predictor()
 
 @app.after_request
 def after_request(response):
-  	response.headers.add('Access-Control-Allow-Origin', '*')
+  	response.headers.add('Access-Control-Allow-Origin', 'https://jonrovira.github.io')
   	response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
   	response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
   	return response
