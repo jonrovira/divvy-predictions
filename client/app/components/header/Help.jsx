e668cf6aa54dc9620406812ddc9553c15cf1cb4b
Jon Rovira <jonathanrovira2016@u.northwestern.edu>
2016-07-19 13:41:46 -0400

    added all partials

--- client/app/components/header/Help.jsx
+++ client/app/components/header/Help.jsx
@@ -6,11 +6,7 @@ const Help = () => (
 	<div className="help-text">
 		<h4>Summary</h4>
 		<p>
-			Our tool helps you visualize
-			how many bikes will probably be
-			available at each Divvy bike sharing
-			station in Chicago within a current
-			3-hour block.
+			Our tool predicts how many bikes will be rented from each Divvy bike sharing station in Chicago within the current 3-hour time block.
 		</p>
 	</div>
 );
